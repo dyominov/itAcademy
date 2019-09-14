5337edca32d17eea817cdbb1617fe80363831fec
dyominov <ersamsungc2500>
2019-09-14 10:34:34 +0300

    task 1 done

---
+++ com/dyominov/itAcademy/task1/PrimeWithManyCollections.java
@@ -0,0 +1,32 @@
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class PrimeWithManyCollections extends Thread {
+    final ArrayBlockingQueue<Integer> primeList;
+    private final int start;
+    private final int end;
+
+    PrimeWithManyCollections(int start, int end, ArrayBlockingQueue<Integer> primeList) {
+        this.start = start;
+        this.end = end;
+        this.primeList = primeList;
+    }
+
+    @Override
+    public void run() {
+        for (int n = Math.max(2, start); n <= end; n++) {
+            boolean prime = true;
+            for (int j = 2; j <= n / 2; j++) {
+                if (n % j == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+
+            if (prime) {
+                primeList.add(n);
+
+            }
+
+        }
+    }
+}
